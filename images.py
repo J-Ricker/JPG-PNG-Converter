451f53ec7b7f8009d1fa05defd20abb1fb0dc7bb
Jeremy Ricker <jrickerdev@gmail.com>
2021-06-02 06:39:51 -0400

    images.py finished for testing PIllow library, thumbnail.jpg resized, conv file for real project

---
+++ images.py
@@ -0,0 +1,17 @@
+from PIL import Image, ImageFilter
+
+
+#img = Image.open('./pokedex/squirtle.jpg')
+#img = Image.open('./astro/astro.jpg')
+#filtered_img = img.filter(ImageFilter.SHARPEN)
+#filtered_img = img.convert('L')
+
+#img.thumbnail((400, 400))
+# img.save('Thumbnail.jpg')
+
+#crooked = filtered_img.rotate(90)
+#resize = filtered_img.resize((300, 300))
+#box = (100, 100, 400, 400)
+#region = filtered_img.crop(box)
+#region.save('cropped.png', 'png')
+# filtered_img.show()
